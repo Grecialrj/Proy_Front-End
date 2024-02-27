7d8497a9439f3ede512fd2a94e046ff312957a3b
Grecialrj <141975434+Grecialrj@users.noreply.github.com>
2024-02-27 01:15:25 -0600

    my first commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+function search() {
+    var searchTerm = document.getElementById('searchInput').value;
+    alert('Buscar: ' + searchTerm);
+    // Aquí puedes realizar la lógica de búsqueda real según tus necesidades
+  }
